65045d1ece8e76175df33d9e55e4d3c2356366f2
dgaillard <dgaillard@jahia.com>
2017-06-07 14:44:28 +0200

    DMF-1446 fix selection issue by creating two filters and update angular material version

--- src/main/resources/javascript/jahia-oauth/app.js
+++ src/main/resources/javascript/jahia-oauth/app.js
@@ -25,4 +25,50 @@
 
         $mdThemingProvider.setDefaultTheme('JahiaOAuthApp');
     }
+
+    angular.module('JahiaOAuthApp').filter('selectable', selectableFilter);
+
+    function selectableFilter() {
+        function isNotMappedOrCurrentlySelected(fieldName, params) {
+            var isNotMappedOrCurrentlySelected = true;
+            if (params.selected && params.selected.name == fieldName) {
+                return isNotMappedOrCurrentlySelected;
+            } else {
+                angular.forEach(params.mapping, function (entry) {
+                    if (entry[params.key] && entry[params.key].name == fieldName) {
+                        isNotMappedOrCurrentlySelected = false;
+                    }
+                });
+            }
+            return isNotMappedOrCurrentlySelected;
+        }
+
+        return function (options, params) {
+            if (options.length == 0 || params.mapping.length == 0) {
+                return options;
+            } else {
+                var newOptions = [];
+                angular.forEach(options, function(option) {
+                    if (isNotMappedOrCurrentlySelected(option.name, params)) {
+                        newOptions.push(option);
+                    }
+                });
+                return newOptions;
+            }
+        }
+    }
+
+    angular.module('JahiaOAuthApp').filter('typeMatch', typeMatchFilter);
+
+    function typeMatchFilter() {
+        return function (options, type) {
+            var newOptions = [];
+            angular.forEach(options, function(option) {
+                if (option.valueType == type) {
+                    newOptions.push(option);
+                }
+            });
+            return newOptions;
+        }
+    }
 })();
